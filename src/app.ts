99139fc5eae280cb04003b01b138bd8af522e378
hansin91 <hansinsusatya@gmail.com>
2024-04-07 19:35:31 +0700

    Clear unused codes

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,10 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
 import { databaseConnection } from './database';
 import { start } from './server';
 
 const initialize = (): void => {
   databaseConnection();
   const app: Express = express();
-  app.get('/', (_: Request, res: Response) => {
-    res.send('Express');
-  });
   start(app);
 };
 
